b50315c3a425ae7603d608a03005f24cf40e31fa
vadim_budarin <budarin-vv@yandex.ru>
2023-04-25 21:01:23 +0300

    add store test

---
+++ src/redux/domain/__tests__/store.test.ts
@@ -0,0 +1,24 @@
+import { initialState, useStore } from '../../store.ts';
+
+describe('useStore', () => {
+    it('should initialize with an empty todo list', () => {
+        const state = useStore.getState();
+        expect(state.todos).toEqual(initialState.todos);
+    });
+
+    it('should add a new todo to the list', () => {
+        // useStore.getState().addTodo('Buy milk');
+        // const updatedState = useStore.getState();
+        // expect(updatedState.todos).toEqual([{ id: 1, text: 'Buy milk' }]);
+    });
+
+    it('should remove a todo from the list', () => {
+        // const initialState = useStore.getState();
+        // useStore.getState().addTodo('Buy milk');
+        // useStore.getState().addTodo('Clean the room');
+        // useStore.getState().removeTodoById(1);
+        // const updatedState = useStore.getState();
+        // expect(updatedState.todos).toEqual([{ id: 2, text: 'Clean the room' }]);
+        // expect(updatedState).toEqual(initialState);
+    });
+});
