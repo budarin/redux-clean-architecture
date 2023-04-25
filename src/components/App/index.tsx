a3bb4dd5b3af6054e8b0808f7342d9c9c0efaeac
vadim_budarin <budarin-vv@yandex.ru>
2023-04-25 17:03:48 +0300

    add data from store

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useStore } from '../../redux/store.ts';
+import { deleteTodo } from '../../redux/domain/todos/index.ts';
 
 function App() {
+    // @ts-ignore
+    const dispatch = useStore((state) => state.dispatch);
+    const todos = useStore((state) => state.todos);
+
+    useEffect(() => {
+        dispatch({
+            type: 'UPDATE_ENTITIES',
+            payload: {
+                todos: [
+                    {
+                        id: 4,
+                        todo: 'Clean room',
+                        completed: false,
+                        deleted: false,
+                    },
+                ],
+            },
+        });
+    }, []);
+
     return (
         <>
             <div>App</div>
+            <div>{JSON.stringify(todos)}</div>
         </>
     );
 }
