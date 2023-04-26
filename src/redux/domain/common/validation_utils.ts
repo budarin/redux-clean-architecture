daba164a2d6fcb2ea507001fecc940a36e523bd8
Vadim Budarin <budarin-vv@yandex.ru>
2023-04-26 04:10:36 +0300

    refactor validation

--- src/redux/domain/common/validation_utils.ts
+++ src/redux/domain/common/validation_utils.ts
@@ -1,7 +1,7 @@
+// валидаторы
 export const isExists = (x: unknown): boolean => Boolean(x);
 export const isNotExists = (x: unknown): boolean => !isExists(x);
 export const isUndefined = (x: any): boolean => x === undefined;
-
 export const isString = (x: unknown): x is string => typeof x === 'string';
 export const isNumber = (x: unknown): x is number => typeof x === 'number';
 export const isNumberLike = (x: unknown): boolean =>
@@ -10,3 +10,30 @@ export const isInt = (x: unknown): x is number => typeof x === 'number' && Numbe
 export const isFloat = (x: unknown): x is number => typeof x === 'number' && Number.isInteger(x) === false;
 export const isBoolean = (x: unknown): x is number => typeof x === 'boolean';
 export const inRange = (num: number, min: number, max: number): boolean => num >= min && num <= max;
+export const isDate = (x: unknown) => isString(x) && Number.isNaN(Date.parse(x)) === false;
+
+// конверторы
+export const toTimeStamp = (x: string | number): number => {
+    if (isString(x)) {
+        return Date.parse(x);
+    }
+    return x as number;
+};
+export const toInt = (x: string | number): number => {
+    
+
+    if (isString(x)) {
+        return parseInt(x, 10);
+    }
+    return x;
+};
+export const toIntOrUndefined = (x: string | number | undefined): number | undefined => {
+    if (isNotExists(x)) {
+        return;
+    }
+    if (isString(x)) {
+        return parseInt(x, 10);
+    }
+
+    return x;
+};
--- src/redux/domain/todos/validators.ts
+++ src/redux/domain/todos/validators.ts
@@ -1,4 +1,4 @@
-import { inRange, isBoolean, isInt, isString, isUndefined } from '../common/validation_utils';
+import { inRange, isBoolean, isInt, isString, isUndefined, toInt, toIntOrUndefined } from '../common/validation_utils';
 
 const MIN_TODO_LENGTH = 5;
 const MAX_TODO_LENGTH = 150;
@@ -7,100 +7,96 @@ const MIN_DESCRIPTION_LENGTH = 10;
 const MAX_DESCRIPTION_LENGTH = 1000;
 
 // обязан присутствовать id типа number
-export const validateId = (x: unknown): boolean => isInt(x);
+export const validateId = ({ id }: Record<string, unknown>): boolean => isInt(id);
 
 // обязан присутствовать status_id типа number
-export const validateStatusId = (x: unknown): boolean => isInt(x);
+export const validateStatusId = ({ status_id }: Record<string, unknown>): boolean => isInt(status_id);
 
 // Category_id должен быть типа number
-export const validateCategoryId = (x: unknown): boolean => isUndefined(x) || isInt(x);
+export const validateCategoryId = ({ x: category_id }: Record<string, unknown>): boolean =>
+    isUndefined(category_id) || isInt(category_id);
 
 // длина todo должна быть более 5 символов и не превышать 150 символов
-function validateTodo(x: unknown): boolean {
-    if (isString(x)) {
-        return inRange(x.length, MIN_TODO_LENGTH, MAX_TODO_LENGTH);
+function validateTodo({ x: todo }: Record<string, unknown>): boolean {
+    if (isString(todo)) {
+        return inRange(todo.length, MIN_TODO_LENGTH, MAX_TODO_LENGTH);
     }
     return false;
 }
 
 // длина description должна быть более 5 символов и не превышать 1000 символов
-export function validateDescription(x: unknown): boolean {
-    if (isUndefined(x)) {
+export function validateDescription({ x: description }: Record<string, unknown>): boolean {
+    if (isUndefined(description)) {
         return true;
     }
 
-    if (isString(x)) {
-        return x.length > MIN_DESCRIPTION_LENGTH && x.length <= MAX_DESCRIPTION_LENGTH;
+    if (isString(description)) {
+        return description.length > MIN_DESCRIPTION_LENGTH && description.length <= MAX_DESCRIPTION_LENGTH;
     }
 
     return false;
 }
 
 // completed должно быть boolean
-export const validateCompleted = (x: unknown): boolean => isBoolean(x);
+export const validateCompleted = (x: Record<string, unknown>): boolean => isBoolean(x);
 
 // deleted должно быть boolean
-export const validateDeleted = (x: unknown): boolean => isUndefined(x) || isBoolean(x);
+export const validateDeleted = (x: Record<string, unknown>): boolean => isUndefined(x) || isBoolean(x);
 
-export function validateTodoObject(input: Record<string, any>): { error: Error[] } | { todo: Todo } {
-    const error = [];
+export function getTodo(input: Record<string, unknown>): Todo {
+    return {
+        id: toInt(input['id'] as NumberLike),
+        status_id: toInt(input['status_id'] as NumberLike),
+        category_id: toIntOrUndefined(input['category_id'] as NumberLike | undefined),
+        todo: input['todo'] as TodoTodo,
+        description: input['description'] as Description,
+        due_date: input['due_date'] as DueDate,
+        deleted: (input['deleted'] || false) as Deleted,
+        completed: input['completed'] as Completed,
+    };
+}
 
-    if (!validateId(input['id'])) {
-        error.push(new Error('Todo обязан иметь id целым числомr'));
+export function validateTodoObject(x: Record<string, unknown>): { valid: boolean; errors: Error[] } {
+    const errors = [];
+
+    if (!validateId(x)) {
+        errors.push(new Error('Todo обязан иметь id целым числомr'));
     }
 
-    if (!validateStatusId(input['status_id'])) {
-        error.push(new Error('Todo обязан иметь status_id целым числом'));
+    if (!validateStatusId(x)) {
+        errors.push(new Error('Todo обязан иметь status_id целым числом'));
     }
 
-    if (!validateCategoryId(input['category_id'])) {
-        error.push(new Error('Category_id должен быть целым числом'));
+    if (!validateCategoryId(x)) {
+        errors.push(new Error('Category_id должен быть целым числом'));
     }
 
-    if (!validateTodo(input['todo'])) {
-        error.push(
+    if (!validateTodo(x)) {
+        errors.push(
             new Error(
                 `Длина названия todo должна быть более ${MIN_TODO_LENGTH} символов и не превышать ${MAX_TODO_LENGTH} символов`,
             ),
         );
     }
 
-    if (!validateDescription(input['description'])) {
-        error.push(
+    if (!validateDescription(x)) {
+        errors.push(
             new Error(
                 `Длина description должна быть более ${MIN_DESCRIPTION_LENGTH} символов и не превышать ${MAX_DESCRIPTION_LENGTH} символов`,
             ),
         );
     }
 
-    if (!validateCompleted(input['completed'])) {
-        error.push(new Error('Completed должно быть boolean'));
+    if (!validateCompleted(x)) {
+        errors.push(new Error('Completed должно быть boolean'));
     }
 
-    if (!validateDeleted(input['deleted'])) {
-        error.push(new Error('Deleted должно быть boolean'));
-    }
-
-    if (error.length > 0) {
-        return {
-            error,
-        };
+    if (!validateDeleted(x)) {
+        errors.push(new Error('Deleted должно быть boolean'));
     }
 
     return {
-        todo: getTodo(input),
-    };
-}
-
-export function getTodo(input: Record<string, any>): Todo {
-    return {
-        id: input['id'],
-        status_id: input['status_id'],
-        category_id: input['category_id'],
-        todo: input['todo'],
-        description: input['description'],
-        due_date: input['due_date'],
-        deleted: input['deleted'] || false,
-        completed: input['completed'],
+        valid: errors.length === 0,
+        errors,
     };
 }
