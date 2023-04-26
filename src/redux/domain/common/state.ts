8dbafabb93a59776811fd4151d5c2073ff2df46c
vadim_budarin <budarin-vv@yandex.ru>
2023-04-26 19:17:00 +0300

    add storeInitialState

--- src/redux/domain/common/state.ts
+++ src/redux/domain/common/state.ts
@@ -3,3 +3,10 @@ export const entityInitialState = {
     byId: {},
     ids: [],
 };
+
+export const storeInitialState = {
+    icons: entityInitialState,
+    statuses: entityInitialState,
+    categories: entityInitialState,
+    todos: entityInitialState,
+};
