02313a1c08cabdfca1b8c4612861dc5b1f5af3c7
vadim_budarin <budarin-vv@yandex.ru>
2023-04-26 19:18:01 +0300

    add RESET action handler to the roor reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,21 +1,29 @@
 import { create } from 'zustand';
 import { redux } from 'zustand/middleware';
+import { storeInitialState } from './domain/common/state.ts';
+
+// actions
+import { RESET_STATE } from './domain/common/actions.ts';
 
 // middlewares
 import { logger } from './middlewares/logger.ts';
 import { businessLogic } from './middlewares/businessLogic.ts';
 
 // reducers
-import todos from './domain/todos/index.ts';
-import categories from './domain/categories/index.ts';
-import statuses from './domain/statuses.ts';
 import icons from './domain/icons.ts';
+import todos from './domain/todos/index.ts';
+import statuses from './domain/statuses.ts';
+import categories from './domain/categories/index.ts';
 
 // порядок редьюсеров очень важен при обновлении всех сущностей
 // с action UPDATE_ENTITIES - делаем вставку в сущности по мереи их
 // зависимостей друг от друга чтобы при проверках не нарушалась
 // ссылочная целостность данных
 function rootReducer(state: State | undefined, action: any) {
+    if (action.type === RESET_STATE) {
+        return storeInitialState;
+    }
+
     return {
         icons: icons(state?.icons, action),
         statuses: statuses(state?.statuses, action),
@@ -26,7 +34,7 @@ function rootReducer(state: State | undefined, action: any) {
 
 export const initialState = rootReducer(undefined, {});
 
-const coreStore = businessLogic(redux(rootReducer, initialState));
+const coreStore = businessLogic(redux(rootReducer, storeInitialState));
 const store = process.env['NODE_ENV'] === 'production' ? coreStore : logger(coreStore);
 
 export const useStore = create<StateEithDispatch>(store);
