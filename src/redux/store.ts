e99ed48f93a0e3f7c1d2836bf1244ebf11851fd0
vadim_budarin <budarin-vv@yandex.ru>
2023-04-26 17:28:11 +0300

    refactor root reduces: add categories

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -7,10 +7,18 @@ import { businessLogic } from './middlewares/businessLogic.ts';
 
 // reducers
 import todos from './domain/todos/index.ts';
+import categories from './domain/categories/index.ts';
 
-function rootReducer(state = {} as State, action: any) {
+// порядок редьюсеров очень важен при обновлении всех сущностей
+// с action UPDATE_ENTITIES - делаем вставку в сущности по мереи их
+// зависимостей друг от друга чтобы при проверках не нарушалась
+// ссылочная целостность данных
+function rootReducer(state: State | undefined, action: any) {
     return {
-        todos: todos(state.todos, action),
+        // icons
+        // statuses
+        categories: categories(state?.categories, action),
+        todos: todos(state?.todos, action),
     };
 }
 
