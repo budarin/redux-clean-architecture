0069e16177633e9393fe3f2a9de58613513efb02
vadim_budarin <budarin-vv@yandex.ru>
2023-04-29 10:52:15 +0300

    fix initial state

--- src/client/redux/store.ts
+++ src/client/redux/store.ts
@@ -19,7 +19,7 @@ import todos from './domain/entities/todos/index.ts';
 import statuses from './domain/entities/statuses/statuses.ts';
 import categories from './domain/entities/categories/index.ts';
 
-import { initialState } from '../../server/initialSate.ts';
+import { initialState as serverInitialState } from '../../server/initialSate.ts';
 import { normalizeEntitiesPayload } from './domain/common/normalizeEntitiesPayload.ts';
 
 function rootReducer(state: EntitiesState | undefined, action: any = {}) {
@@ -38,7 +38,9 @@ function rootReducer(state: EntitiesState | undefined, action: any = {}) {
         todos: todos(state?.todos, action),
     };
 }
-const coreStore = businessLogic(redux(rootReducer, normalizeEntitiesPayload(initialState) as State));
+
+const initialState = normalizeEntitiesPayload(serverInitialState) as State;
+const coreStore = businessLogic(redux(rootReducer, initialState));
 const store = process.env['NODE_ENV'] === 'production' ? coreStore : logger(coreStore);
 
 export const useStore = create<State>(store);
