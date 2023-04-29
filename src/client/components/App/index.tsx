563f5a62c1022492e0b74176b89e9cb98b1e94e9
vadim_budarin <budarin-vv@yandex.ru>
2023-04-29 11:00:12 +0300

    refactor how to set initial state

--- src/client/components/App/index.tsx
+++ src/client/components/App/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useStore } from '../../redux/store.ts';
+import { initialState } from '../../../server/initialSate.ts';
 
 function App() {
+    const dispatch = useStore((state) => state.dispatch);
     const todos = useStore((state) => state);
+
+    useEffect(() => {
+        dispatch({
+            type: 'UPDATE',
+            payload: {
+                entities: initialState,
+            },
+        });
+    }, []);
 
     return (
         <>
--- src/client/redux/store.ts
+++ src/client/redux/store.ts
@@ -19,9 +19,6 @@ import todos from './domain/entities/todos/index.ts';
 import statuses from './domain/entities/statuses/statuses.ts';
 import categories from './domain/entities/categories/index.ts';
 
-import { initialState as serverInitialState } from '../../server/initialSate.ts';
-import { normalizeEntitiesPayload } from './domain/common/normalizeEntitiesPayload.ts';
-
 function rootReducer(state: EntitiesState | undefined, action: any = {}) {
     if (typeof action !== 'object') {
         return state;
@@ -39,8 +36,7 @@ function rootReducer(state: EntitiesState | undefined, action: any = {}) {
     };
 }
 
-const initialState = normalizeEntitiesPayload(serverInitialState) as State;
-const coreStore = businessLogic(redux(rootReducer, initialState));
+const coreStore = businessLogic(redux(rootReducer, getEntitiesInitialState()));
 const store = process.env['NODE_ENV'] === 'production' ? coreStore : logger(coreStore);
 
 export const useStore = create<State>(store);
