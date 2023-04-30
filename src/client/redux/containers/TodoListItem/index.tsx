0894e2daa8d5301afe2d03996b7d7590e024f317
vadim_budarin <budarin-vv@yandex.ru>
2023-04-30 08:43:50 +0300

    fix typings

--- src/client/redux/containers/TodoListItem/index.tsx
+++ src/client/redux/containers/TodoListItem/index.tsx
@@ -7,10 +7,10 @@ import TodoListItem from '../../../components/TodoListItem/index.tsx';
 
 const TodoListItemContainer = React.memo(({ id }: { id: number }) => {
     const dispatch = useStore((state) => state.dispatch);
-    const todo = useStore((state) => state.todos.byId[id as keyof typeof state.todos.byId]);
-    const status = useStore((state) => state.statuses.byId[todo.status_id as keyof typeof state.statuses.byId]);
-    const category = useStore((state) => state.categories.byId[todo.category_id as keyof typeof state.categories.byId]);
-    const icon = useStore((state) => state.icons.byId[category.icon_id as keyof typeof state.icons.byId]);
+    const todo = useStore((state) => state.todos.byId[id as TodosIndex]);
+    const category = useStore((state) => state.categories.byId[todo.category_id as CategoriesIndex]);
+    const icon = useStore((state) => state.icons.byId[category.icon_id as IconsIndex]);
+    const status = useStore((state) => state.statuses.byId[todo.status_id as StatusesIndex]);
 
     const handleUpdate = React.useCallback(
         (updatedTodo: string) => {
