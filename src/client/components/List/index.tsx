e7a35fb45e0ce7c76484d98c3690b34f76f8a83b
Vadim Budarin <budarin-vv@yandex.ru>
2023-05-01 08:19:03 +0300

    add cheader

--- src/client/components/List/index.tsx
+++ src/client/components/List/index.tsx
@@ -1,11 +1,17 @@
 import React, { ReactNode } from 'react';
 
 type ListProps = {
+    category: string;
     children: ReactNode;
 };
 
-function List({ children }: ListProps) {
-    return <ul>{children}</ul>;
+function List({ category, children }: ListProps) {
+    return (
+        <>
+            <h3>{category}</h3>
+            <ul>{children}</ul>
+        </>
+    );
 }
 
 export default List;
